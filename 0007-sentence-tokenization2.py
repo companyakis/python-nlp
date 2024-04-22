2d33557ecac4da573b3e16817349064d703fddf4
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-04-22 12:38:05 +0300

    Create 0007-sentence-tokenization2.py

---
+++ 0007-sentence-tokenization2.py
@@ -0,0 +1,9 @@
+sentence = "When he was young, his pocket was empty. A barking dog never bites. Go away!"
+
+tokens = sent_tokenize(sentence)
+
+print(tokens)
+
+"""
+['When he was young, his pocket was empty.', 'A barking dog never bites.', 'Go away!']
+"""
